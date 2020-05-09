6ac7f3c1bc7ce93656df6f39fb54fd515a6e30da
jeromedecoster <jeromedecoster@users.noreply.github.com>
2020-05-09 23:54:10 +0200

    :boom:

--- install.sh
+++ install.sh
@@ -1,22 +1,25 @@
 set -e
 
-log() { echo -e "\e[38;5;82;4m${1}\e[0m \e[38;5;226m${@:2}\e[0m"; }
-err() { echo -e "\e[38;5;196;4m${1}\e[0m \e[38;5;87m${@:2}\e[0m" >&2; }
+log()   { echo -e "\e[30;47m ${1^^} \e[0m ${@:2}"; }
+info()  { echo -e "\e[48;5;28m ${1^^} \e[0m ${@:2}"; }
+warn()  { echo -e "\e[48;5;202m ${1^^} \e[0m ${@:2}" >&2; }
+error() { echo -e "\e[48;5;196m ${1^^} \e[0m ${@:2}" >&2; }
 
+info install down
 cd /usr/local/bin
 
 # abort if already installed
-[[ -x down ]] && { log abort down already exists; exit 0; }
+[[ -x down ]] && { error abort down already exists; exit 0; }
 
 # ask sudo accesss if not already available
 if [[ -z $(sudo -n uptime 2>/dev/null) ]]; then
-    log warn sudo access required
+    warn warn sudo access required
     sudo echo >/dev/null
     # one more check if the user abort the password question
-    [[ -z `sudo -n uptime 2>/dev/null` ]] && { err abort sudo required; exit 1; }
+    [[ -z `sudo -n uptime 2>/dev/null` ]] && { error abort sudo required; exit 1; }
 fi
 
-log install down
+log download down
 if [[ -n $(which curl) ]]
 then
     sudo curl raw.githubusercontent.com/jeromedecoster/down/master/down \
@@ -32,5 +35,5 @@ fi
 
 sudo chmod +x down
 
-log complete down successfully installed
+info installed down
 exit 0
